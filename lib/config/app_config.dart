travka/fluffychat

refactor: New logo

lib/config/app_config.dart
@@ -7,7 +7,7 @@ import 'dart:ui';
 
 abstract class AppConfig {
   // Const and final configuration values (immutable)
-  static const Color primaryColor = Color(0xFF5625BA);
+  static const Color primaryColor = Color(0xFF6666ff);
   static const Color primaryColorLight = Color(0xFFCCBDEA);
   static const Color secondaryColor = Color(0xFF41a2bc);
 

tjena-eu/tjena_chat

lib/config/app_config.dart
@@ -6,10 +6,7 @@
 import 'dart:ui';
 
 abstract class AppConfig {
-  // Const and final configuration values (immutable)
-  static const Color primaryColor = Color(0xFF5625BA);
-  static const Color primaryColorLight = Color(0xFFCCBDEA);
-  static const Color secondaryColor = Color(0xFF41a2bc);
+  static const Color primaryColor = Color(0xFF41a2bc);
 
   static const Color chatColor = primaryColor;
   static const double messageFontSize = 16.0;